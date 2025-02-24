b0fd25632c346bfea8f2809596aadce2565fd6f8
Martin Doušek <dousek.martin@gmail.com>
2025-02-24 13:56:21 +0000

    fix index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
 // Middleware
 app.use(express.json()); // To parse JSON request bodies
