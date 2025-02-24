01038b56bafba3c11e67eb3ea3d1f0f188bdfdf6
Martin Doušek <dousek.martin@gmail.com>
2025-02-24 13:21:36 +0000

    Fix docker-compose

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(express.json()); // To parse JSON request bodies
